7fe9eb03ecbef46accae0157af6b8547a585b5e8
the-robot <the-robot@users.noreply.github.com>
2020-02-28 15:37:43 +0800

    updated http exception

--- kelpie/exceptions.py
+++ kelpie/exceptions.py
@@ -15,6 +15,10 @@ class HttpException(Exception):
             content=self.details,
         )
 
+    def __repr__(self) -> str:
+        class_name = self.__class__.__name__
+        return f"{class_name}(status_code={self.status_code!r}, detail={self.details!r})"
+
     def __str__(self) -> str:
         class_name = self.__class__.__name__
         return f"{class_name}(status_code={self.status_code!r}, detail={self.details!r})"
