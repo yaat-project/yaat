45ba6184f304181d14beb621ac709f8660074461
the-robot <the-robot@users.noreply.github.com>
2020-02-28 16:20:43 +0800

    Added custom handler; template example

--- app.py
+++ app.py
@@ -1,18 +1,28 @@
 from kelpie import Kelpie
-from kelpie.responses import PlainTextResponse
+from kelpie.responses import HTMLResponse, PlainTextResponse
 
 
 # NOTE: Example Web Application To Test Kelpie
-app = Kelpie()
+app = Kelpie(templates_dir="templates")
 
 
+# NOTE: templating
+@app.route("/home")
+async def home(req):
+    return HTMLResponse(
+        content=app.template("home.html", context={
+            "name": "Kelpine"
+        })
+    )
+
+
+# NOTE: Decorate Route
 @app.route("/hello/{name}")
 async def hello(req, name):
     response = PlainTextResponse(
         content=f"Hello World, {name}"
     )
     return response
-
 
 @app.route("/about")
 async def about(req):
@@ -22,6 +32,7 @@ async def about(req):
     return response
 
 
+# NOTE: Class Route
 @app.route("/books")
 class BookHandler:
     async def get(self, req):
@@ -35,3 +46,28 @@ class BookHandler:
             content=f"Create Book"
         )
         return response
+
+
+# NOTE: Add route by method
+async def manual_add_route(req):
+    response = PlainTextResponse(
+        content=f"Manual Add Route"
+    )
+    return response
+
+app.add_route("/manual_route", manual_add_route)
+
+
+# NOTE: Custom Exception
+# def custom_exception_handler(req, exception_cls) -> PlainTextResponse:
+#     response = PlainTextResponse(
+#         status_code=500,
+#         content=f"Got server error, please contact our tech support!"
+#     )
+#     return response
+
+# app.exception_handler = custom_exception_handler
+
+@app.route("/exception")
+def exception_throwing_handler(req):
+    raise AssertionError("This handler should not be user")
