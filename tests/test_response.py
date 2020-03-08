ce34e3c0dc83d9e15ccc5e3fe5f8b709941c4cdf
the-robot <9334746+the-robot@users.noreply.github.com>
2020-03-08 21:42:21 +0000

    added plain text response

--- tests/test_response.py
+++ tests/test_response.py
@@ -1,7 +1,24 @@
 import pytest
 
 from alicorn import Alicorn
-from alicorn.responses import JSONResponse
+from alicorn.responses import (
+    JSONResponse,
+    PlainTextResponse,
+)
+
+
+@pytest.mark.asyncio
+async def test_plain_text_response(app, client):
+    RESPONSE = "hello world"
+
+    @app.route("/text")
+    async def plain_text_handler(request):
+        return PlainTextResponse(content=RESPONSE)
+
+    res = await client.get("/text")
+
+    assert res.text == RESPONSE
+    assert res.status_code == 200
 
 
 @pytest.mark.asyncio
@@ -16,4 +33,5 @@ async def test_json_response(app, client):
 
     assert res.json() == RESPONSE
     assert res.status_code == 200
+
 
