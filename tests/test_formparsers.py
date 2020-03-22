387e4db74efdcd3f8ec2f88276bfb40d2661cf57
the-robot <9334746+the-robot@users.noreply.github.com>
2020-03-22 08:23:21 +0000

    added formparser unit tests

--- tests/test_formparsers.py
+++ tests/test_formparsers.py
@@ -1,43 +1,79 @@
 import pytest
+import tempfile
+
+from alicorn.responses import JSONResponse
+
+
+class ForceMultipart(dict):
+    def __bol__(self):
+        return True
+
+FORCE_MULTIPART = ForceMultipart()
 
 
 @pytest.mark.asyncio
-async def test_file_upload(app, client):
+async def test_read_body(app, client):
+    @app.route("/")
+    async def handler(request):
+        await request.body()
+        form = await request.form()
+        response_data = dict(form)
+        return JSONResponse(response_data)
+
+    res = await client.post("/", data={"abc": "123"})
+    assert res.json() == {"abc": "123"}
+
+
+@pytest.mark.asyncio
+async def test_data(app, client):
+    @app.route("/")
+    async def handler(request):
+        form = await request.form()
+        response_data = dict(form)
+        return JSONResponse(response_data)
+
+    res = await client.post("/", data={"abc": "123"}, files=FORCE_MULTIPART)
+    assert res.json() == {"abc": "123"}
+
+
+@pytest.mark.asyncio
+async def test_upload_files(app, client, tmpdir):
+    CONTENT = b"<file_content>"
+
+    temp = tempfile.NamedTemporaryFile(dir=tmpdir, suffix='.txt', delete=False)
+    temp.write(CONTENT)
+    temp.close()
+    filename = temp.name.split("/")[-1]
+
+    @app.route("/")
+    async def handler(request):
+        form = await request.form()
+        upload = form.get("test")
+
+        response_data = {
+            "filename": upload.name,
+            "content": (await upload.read()).decode("utf-8"),
+            "content_type": upload.content_type
+        }        
+
+        return JSONResponse(response_data)
+
+    with open(temp.name, "rb") as f:
+        res = await client.post("/", files={"test": f})
+        assert res.json() == {
+            "filename": filename,
+            "content_type": "text/plain",
+            "content": "<file_content>",
+        }
+
+
+@pytest.mark.asyncio
+async def test_multiple_upload_files(app, client, tmpdir):
     pass
 
 
 @pytest.mark.asyncio
 async def test_multi_items(app, client):
-    pass
-
-
-@pytest.mark.asyncio
-async def test_read_body(app, client):
-    pass
-
-
-@pytest.mark.asyncio
-async def test_multipart_request_data(app, client):
-    pass
-
-
-@pytest.mark.asyncio
-async def test_multipart_request_files(app, client, tmpdir):
-    pass
-
-
-@pytest.mark.asyncio
-async def test_multipart_request_files_with_content_type(app, client, tmpdir):
-    pass
-
-
-@pytest.mark.asyncio
-async def test_multipart_request_multiple_files(app, client, tmpdir):
-    pass
-
-
-@pytest.mark.asyncio
-async def test_multiple_items(app, client, tmpdir):
     pass
 
 
