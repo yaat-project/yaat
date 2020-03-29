270e42a4aaced564524aba4e83b92ab500ae4e20
the-robot <9334746+the-robot@users.noreply.github.com>
2020-03-29 20:53:06 +0000

    added websocket support

--- yaat/routing.py
+++ yaat/routing.py
@@ -5,11 +5,19 @@ from .constants import HTTP_METHODS
 
 
 class Route:
-    def __init__(self, path: str, handler: callable, methods: list = None, is_static: bool = False):
+    def __init__(
+        self,
+        path: str,
+        handler: callable,
+        methods: list = None,
+        is_static: bool = False,
+        is_websocket: bool = False,
+    ):
         self.path = path
         self.handler = handler
         self.methods = methods if methods else HTTP_METHODS
-        self.is_static = is_static  # check if it is static route
+        self.is_static = is_static  # check if  static route
+        self.is_websocket = is_websocket  # check if websocket route
 
     @property
     def path(self) -> str:
@@ -59,29 +67,55 @@ class Router:
             paths.add(route.path)
         return list(paths)
 
-    def add_route(self, path: str, handler: callable, methods: list = None, is_static: bool = False):
-        assert path not in self.paths, f"Route {path}, already exists"
-        self.routes.append(
-            Route(path=path, handler=handler, methods=methods, is_static=is_static)
-        )
-
     def route(self, path: str, methods: list = None) -> callable:
         def wrapper(handler):
-            self.add_route(path, handler, methods)
+            self.add_route(path=path, handler=handler, methods=methods)
             return handler
-
         return wrapper
 
-    def mount(self, router: callable, prefix: str = None, is_static: bool = False):
+    def add_route(self, path: str, handler: callable, methods: list = None, static: bool = False):
+        assert path not in self.paths, f"Route {path}, already exists"
+        self.routes.append(
+            Route(path=path, handler=handler, methods=methods, is_static=static)
+        )
+
+    def websocket_route(self, path: str) -> callable:
+        def wrapper(handler):
+            self.add_websocket_route(path=path)
+            return handler
+        return wrapper
+
+    def add_websocket_route(self, path: str, handler: callable):
+        assert path not in self.paths, f"Route {path}, already exists"
+        self.routes.append(
+            Route(path=path, handler=handler, is_websocket=True)
+        )
+
+    def mount(
+        self,
+        router: callable,
+        prefix: str = None,
+        static: bool = False,
+        websocket: bool = False
+    ):
         """Mount another router"""
         routes = router.routes
 
         # register sub routes
         for route in routes:
             path = self.__add_prefix(prefix, route.path) if prefix else route.path
-            self.add_route(path=path, handler=route.handler, methods=route.methods, is_static=is_static)
 
-    def get_route(self, request_path: str, method: str) -> (Route, typing.Any):
+            if websocket:
+                self.add_websocket_route(path=path, handler=route.handler)
+            else:
+                self.add_route(
+                    path=path,
+                    handler=route.handler,
+                    methods=route.methods,
+                    static=static,
+                )
+
+    def get_route(self, request_path: str) -> (Route, typing.Any):
         for route in self.routes:
             # for static routing, use different methods for route comparison
             if route.is_static and request_path.startswith(route.path):
---
+++ yaat/websockets.py
@@ -0,0 +1,136 @@
+import enum
+import json
+import typing
+
+from .exceptions import WebSocketDisconnectException
+from .requests import HTTPConnection
+from .types import Message, Scope, Receive, Send
+
+
+# CONSTANTS
+class WebSocketStates(enum.Enum):
+    CONNECTING = 0
+    CONNECTED = 1
+    DISCONNECTED = 2
+
+class WebSocketMessages:
+    ACCEPT = "websocket.accept"
+    CONNECT = "websocket.connect"
+    DISCONNECT = "websocket.disconnect"
+    SEND = "websocket.send"
+    RECEIVE = "websocket.receive"
+    CLOSE = "websocket.close"
+
+
+class WebSocket(HTTPConnection):
+    def __init__(self, scope: Scope, receive: Receive, send: Send):
+        super().__init__(scope)
+        assert scope["type"] == "websocket"
+
+        self._receive = receive
+        self._send = send
+        self.client_state = WebSocketStates.CONNECTING
+        self.application_state = WebSocketStates.CONNECTING
+
+    async def receive(self) -> Message:
+        """
+        Receive ASGI websocket message
+        """
+        if self.client_state == WebSocketStates.CONNECTING:
+            message = await self._receive()
+            assert message["type"] == WebSocketMessages.CONNECT
+            self.client_state = WebSocketStates.CONNECTED
+            return message
+
+        elif self.client_state == WebSocketStates.CONNECTED:
+            message = await self._receive()
+            message_type = message["type"]
+            assert message_type in {WebSocketMessages.RECEIVE, WebSocketMessages.DISCONNECT}
+            if message_type == WebSocketMessages.DISCONNECT:
+                self.cleint_state = WebSocketStates.DISCONNECTED
+            return message
+
+        else:
+            raise RuntimeError("Cannot 'receive' when disconnected")
+
+    async def send(self, message: Message):
+        """
+        Send ASGI websocket message
+        """
+        if self.application_state == WebSocketStates.CONNECTING:
+            message_type = message["type"]
+            assert message_type in {WebSocketMessages.ACCEPT, WebSocketMessages.CLOSE}
+
+            if message_type == WebSocketMessages.CLOSE:
+                self.application_state = WebSocketStates.DISCONNECTED
+            else:
+                self.application_state = WebSocketStates.CONNECTED
+            await self._send(message)
+
+        elif self.application_state == WebSocketStates.CONNECTED:
+            message_type = message["type"]
+            assert message_type in {WebSocketMessages.SEND, WebSocketMessages.CLOSE}
+            if message_type == WebSocketMessages.CLOSE:
+                self.application_state = WebSocketStates.DISCONNECTED
+            await self._send(message)
+
+        else:
+            raise RuntimeError("Cannot 'send' when disconnected")
+
+
+    # Accept/Close connections
+    async def accept(self, subprotocol: str = None):
+        if self.client_state == WebSocketStates.CONNECTING:
+            await self.receive()
+        await self.send({"type": WebSocketMessages.ACCEPT, "subprotocol": subprotocol})
+
+    async def close(self, code: int = 1000):
+        # Websocket status codes
+        # https://developer.mozilla.org/en-US/docs/Web/API/CloseEvent#Status_codes
+        await self.send({"type": WebSocketMessages.CLOSE, "code": code})
+
+
+    # Sending methods
+    async def send_text(self, data: str):
+        await self.send({"type": WebSocketMessages.SEND, "text": data})
+
+    async def send_bytes(self, data: bytes):
+        await self.send({"type": WebSocketMessages.SEND, "bytes": data})
+
+    async def send_json(self, data: typing.Any, mode: str = "text"):
+        assert mode in ["text", "binary"]
+        raw = json.dumps(data)
+
+        if mode == "text":
+            await self.send({"type": "websocket.send", "text": raw})
+        else:
+            await self.send({"type": "websocket.send", "bytes": raw.encode("utf-8")})
+
+
+    # Receiving methods
+    async def receive_text(self) -> str:
+        assert self.application_state == WebSocketStates.CONNECTED
+        message = await self.receive()
+        self.__raise_if_disconnected(message)
+        return message["text"]
+
+    async def receive_bytes(self) -> bytes:
+        assert self.application_state == WebSocketState.CONNECTED
+        message = await self.receive()
+        self.__raise_if_disconnected(message)
+        return message["bytes"]
+
+    async def receive_json(self, mode: str = "text") -> typing.Any:
+        assert mode in ["text", "binary"]
+        assert self.application_state == WebSocketState.CONNECTED
+        message = await self.receive()
+        self.__raise_if_disconnected(message)
+
+        text = message["text"] if mode == "text" else message["bytes"].decode("utf-8")
+        return json.loads(text)
+
+
+    # Exception handlers
+    def __raise_if_disconnected(self, message: Message):
+        if message["type"] == WebSocketMessages.DISCONNECT:
+            raise WebSocketDisconnectException(message["code"])
