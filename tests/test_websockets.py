06fe102ada43f31499d7360f27c12d8fa21a1e23
the-robot <9334746+the-robot@users.noreply.github.com>
2020-03-30 15:50:36 +0000

    initialize websocket unit tests

--- tests/test_websockets.py
+++ tests/test_websockets.py
@@ -1,5 +1,6 @@
 import pytest
 
+from yaat.constants import WebSocketCloseEvent
 from yaat.responses import HTMLResponse
 from yaat.websockets import WebSocket, WebSocketDisconnect
 
@@ -10,3 +11,63 @@ import websocket
 async def test_websocket_url(ws_uri):
     ws = websocket.WebSocket()
     ws.connect(ws_uri)
+
+
+@pytest.mark.asyncio
+async def test_websocket_text(ws_uri):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_websocket_json(ws_uri):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_websocket_bytes(ws_uri):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_websocket_query_params(ws_uri):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_websocket_headers(ws_uri):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_client_close(ws_uri):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_server_close(ws_uri):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_reject_connection(ws_uri):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_subprotocol(ws_uri):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_websocket_exception(ws_uri):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_duplicate_close(ws_uri):
+    pass
+
+
+@pytest.mark.asyncio
+async def test_duplicate_disconnect(ws_uri):
+    pass
