45787176a5fa1f0b30eb05ec58995bb4c5aad845
the-robot <9334746+the-robot@users.noreply.github.com>
2020-03-30 15:26:09 +0000

    configured websocket unit testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,16 @@
+from multiprocessing import Process
 import httpx
 import pytest
-import websocket
+import typing
+import uvicorn
 
 from yaat import Yaat
+from yaat.websockets import WebSocket, WebSocketDisconnect
+
+
+# CONSTANTS
+WS_TEST_SERVER_HOST = 'localhost'
+WS_TEST_SERVER_PORT = 13370
 
 
 @pytest.fixture
@@ -12,3 +20,36 @@ def app() -> Yaat:
 @pytest.fixture
 def client(app) -> httpx.AsyncClient:
     return app.test_client()
+
+@pytest.fixture
+def ws_uri() -> str:
+    return f"ws://{WS_TEST_SERVER_HOST}:{WS_TEST_SERVER_PORT}"
+
+@pytest.fixture(scope="session", autouse=True)
+def ws_background_server() -> typing.AsyncGenerator:
+    """
+        WebSocket server to run in background for
+        websocket unit tests
+    """
+    app = Yaat()
+
+    # Endpoints
+    @app.websocket_route("/")
+    async def handler(websocket: WebSocket):
+        await websocket.accept()
+        await websocket.send_text("Hello")
+        await websocket.close()
+
+
+    # run server with uvicorn as daemon
+    process = Process(
+        target=lambda: uvicorn.run(
+            app,
+            host=WS_TEST_SERVER_HOST,
+            port=WS_TEST_SERVER_PORT,
+        ),
+        daemon=True
+    )
+    process.start()
+    yield
+    process.kill()
--- tests/test_websockets.py
+++ tests/test_websockets.py
@@ -1,1 +1,12 @@
 import pytest
+
+from yaat.responses import HTMLResponse
+from yaat.websockets import WebSocket, WebSocketDisconnect
+
+import websocket
+
+
+@pytest.mark.asyncio
+async def test_websocket_url(ws_uri):
+    ws = websocket.WebSocket()
+    ws.connect(ws_uri)
