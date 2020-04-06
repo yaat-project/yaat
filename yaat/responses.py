28285a41e1a28f1f71e2a6d5d62fb97bf555dcbf
the-robot <9334746+the-robot@users.noreply.github.com>
2020-04-06 19:43:05 +0800

    Travis (#8)
    - Travis setup

--- yaat/responses.py
+++ yaat/responses.py
@@ -171,7 +171,7 @@ class FileResponse(Response):
         self.status_code = status_code
         self.send_header_only = method is not None and method.upper() == "HEAD"
         if media_type is None:
-            media_type = guess_type(filename or path)[0] or "text/plain"
+            media_type = guess_type(filename or str(path))[0] or "text/plain"
         self.headers = headers if headers is not None else {}
         self.media_type = media_type
         self.stat_result = stat_result
