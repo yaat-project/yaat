4150aaad83231831392adb8e62121f64a0240e94
ghost <9334746+the-robot@users.noreply.github.com>
2020-04-06 19:29:19 +0800

    enabled websocket tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -26,66 +26,66 @@ def client(app) -> httpx.AsyncClient:
 def ws_uri() -> str:
     return f"ws://{WS_TEST_SERVER_HOST}:{WS_TEST_SERVER_PORT}"
 
-# @pytest.fixture(scope="session", autouse=True)
-# def ws_background_server() -> typing.AsyncGenerator:
-#     """
-#         WebSocket server to run in background for
-#         websocket unit tests
-#     """
-#     app = Yaat()
+@pytest.fixture(scope="session", autouse=True)
+def ws_background_server() -> typing.AsyncGenerator:
+    """
+        WebSocket server to run in background for
+        websocket unit tests
+    """
+    app = Yaat()
 
-#     # Endpoints
-#     @app.websocket_route("/hello")
-#     async def hello_handler(websocket: WebSocket):
-#         await websocket.accept()
-#         await websocket.send_text("Hello")
-#         await websocket.close()
+    # Endpoints
+    @app.websocket_route("/hello")
+    async def hello_handler(websocket: WebSocket):
+        await websocket.accept()
+        await websocket.send_text("Hello")
+        await websocket.close()
 
-#     @app.websocket_route("/text")
-#     async def text_handler(websocket: WebSocket):
-#         await websocket.accept()
-#         data = await websocket.receive_text()
-#         await websocket.send_text(f"You sent me '{data}'")
-#         await websocket.close()
+    @app.websocket_route("/text")
+    async def text_handler(websocket: WebSocket):
+        await websocket.accept()
+        data = await websocket.receive_text()
+        await websocket.send_text(f"You sent me '{data}'")
+        await websocket.close()
 
-#     @app.websocket_route("/json")
-#     async def json_handler(websocket: WebSocket):
-#         await websocket.accept()
-#         data = await websocket.receive_json()
-#         await websocket.send_json(data)
-#         await websocket.close()
+    @app.websocket_route("/json")
+    async def json_handler(websocket: WebSocket):
+        await websocket.accept()
+        data = await websocket.receive_json()
+        await websocket.send_json(data)
+        await websocket.close()
 
-#     @app.websocket_route("/bytes")
-#     async def bytes_handler(websocket: WebSocket):
-#         await websocket.accept("binary")
-#         data = await websocket.receive_bytes()
-#         await websocket.send_bytes(data)
-#         await websocket.close()
+    @app.websocket_route("/bytes")
+    async def bytes_handler(websocket: WebSocket):
+        await websocket.accept("binary")
+        data = await websocket.receive_bytes()
+        await websocket.send_bytes(data)
+        await websocket.close()
 
-#     @app.websocket_route("/query-param")
-#     async def query_handler(websocket: WebSocket):
-#         await websocket.accept()
-#         query = dict(websocket.query_params)
-#         await websocket.send_json(query)
-#         await websocket.close()
+    @app.websocket_route("/query-param")
+    async def query_handler(websocket: WebSocket):
+        await websocket.accept()
+        query = dict(websocket.query_params)
+        await websocket.send_json(query)
+        await websocket.close()
 
-#     @app.websocket_route("/headers")
-#     async def headers_handler(websocket: WebSocket):
-#         await websocket.accept()
-#         headers = websocket.headers
-#         await websocket.send_json({"hello": headers["hello"]})
-#         await websocket.close()
+    @app.websocket_route("/headers")
+    async def headers_handler(websocket: WebSocket):
+        await websocket.accept()
+        headers = websocket.headers
+        await websocket.send_json({"hello": headers["hello"]})
+        await websocket.close()
 
 
-#     # run server with uvicorn as daemon
-#     process = Process(
-#         target=lambda: uvicorn.run(
-#             app,
-#             host=WS_TEST_SERVER_HOST,
-#             port=WS_TEST_SERVER_PORT,
-#         ),
-#         daemon=True
-#     )
-#     process.start()
-#     yield
-#     process.kill()
+    # run server with uvicorn as daemon
+    process = Process(
+        target=lambda: uvicorn.run(
+            app,
+            host=WS_TEST_SERVER_HOST,
+            port=WS_TEST_SERVER_PORT,
+        ),
+        daemon=True
+    )
+    process.start()
+    yield
+    process.kill()
