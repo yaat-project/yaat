f38f71096bc940ac4d7da47d759972f10d96e5e3
the-robot <9334746+the-robot@users.noreply.github.com>
2020-04-12 19:55:02 +0000

    Added unit test for cors regex allow origin

--- tests/middleware/test_cors.py
+++ tests/middleware/test_cors.py
@@ -90,7 +90,7 @@ async def test_disallowed_preflight(app, client):
 
 
 @pytest.mark.asyncio
-async def test_cors_credentialed_return_specific_origin(app, client):
+async def test_credentialed_return_specific_origin(app, client):
     app.add_middleware(CorsMiddleware, allow_origins=["*"])
 
     @app.route("/")
@@ -106,7 +106,7 @@ async def test_cors_credentialed_return_specific_origin(app, client):
 
 
 @pytest.mark.asyncio
-async def test_cors_vary_headers_origin_default(app, client):
+async def test_vary_headers_origin_default(app, client):
     app.add_middleware(CorsMiddleware, allow_origins=["http://testserver.com"])
 
     @app.route("/")
@@ -120,7 +120,7 @@ async def test_cors_vary_headers_origin_default(app, client):
 
 
 @pytest.mark.asyncio
-async def test_cors_vary_headers_set(app, client):
+async def test_vary_headers_set(app, client):
     app.add_middleware(CorsMiddleware, allow_origins=["http://testserver.com"])
 
     @app.route("/")
@@ -131,3 +131,48 @@ async def test_cors_vary_headers_set(app, client):
     assert res.status_code == 200
     assert res.headers["access-control-allow-origin"] == "http://testserver.com"
     assert res.headers["vary"] == "User-Agent, Origin"
+
+
+@pytest.mark.asyncio
+async def test_allow_origin_regex(app, client):
+    app.add_middleware(
+        CorsMiddleware,
+        allow_origin_regex="http://.*.com",
+        allow_headers=["X-My-Custom-Header"],
+    )
+
+    @app.route("/")
+    async def handler(request):
+        return TextResponse("hello world")
+
+    # preflight request
+    res = await client.options("/", headers={
+        "Origin": "http://testserver.com",
+        "Access-Control-Request-Method": "GET",
+        "Access-Control-Request-Headers": "X-My-Custom-Header",
+    })
+    assert res.status_code == 200
+    assert res.headers["access-control-allow-origin"] == "http://testserver.com"
+    assert "X-My-Custom-Header" in res.headers["access-control-allow-headers"].split(", ")
+
+    # disallowed preflight request
+    res = await client.options("/", headers={
+        "Origin": "http://testserver.org",
+        "Access-Control-Request-Method": "GET",
+        "Access-Control-Request-Headers": "X-My-Custom-Header",
+    })
+    assert res.status_code == 400
+    assert res.text == "Disallowed cors Origin"
+    assert "access-control-allow-origin" not in res.headers
+
+    # simple request
+    res = await client.get("/", headers={"Origin": "http://testserver.com"})
+    assert res.status_code == 200
+    assert res.text == "hello world"
+    assert res.headers["access-control-allow-origin"] == "http://testserver.com"
+
+    # disallowed simple request
+    res = await client.get("/", headers={"Origin": "http://testserver.org"})
+    assert res.status_code == 200
+    assert res.text == "hello world"
+    assert "access-control-allow-origin" not in res.headers
