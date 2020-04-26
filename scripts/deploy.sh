239abf97c8723d2c77ef875f5f8c897e5eff6e19
the-robot <9334746+the-robot@users.noreply.github.com>
2020-04-26 20:08:03 +0000

    updated travis for auto deployment

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,2 @@
 python setup.py sdist
-# twine upload dist/* --username $PYPI_USER --password $PYPI_SECRET
-
-echo $PYPI_USER
-echo $PYPI_SECRET
+twine upload dist/* --username $PYPI_USER --password $PYPI_SECRET
