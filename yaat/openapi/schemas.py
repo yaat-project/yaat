242f27852a0e906505519246f5e3452e9115e120
the-robot <9334746+the-robot@users.noreply.github.com>
2020-05-09 20:06:42 +0000

    Added functionality to auto add param documentation if not defined

--- yaat/openapi/schemas.py
+++ yaat/openapi/schemas.py
@@ -39,26 +39,6 @@ class SchemaGenerator:
         schema.setdefault("paths", {})
         routes_info = self.get_routes_info(routes)
 
-        def add_schema(
-            schema: typing.Dict,
-            path: str,
-            method: str,
-            handler: typing.Callable,
-            tags: typing.List,
-        ):
-            docs = self.get_docstirng(handler)
-            method = method.lower()
-
-            if len(docs) == 0:
-                return
-
-            if path not in schema["paths"]:
-                schema["paths"][path] = {}
-
-            schema["paths"][path][method] = docs
-            if tags:
-                schema["paths"][path][method]["tags"] = tags
-
         for route in routes_info:
             # if it is a class, check by its class methods
             if inspect.isclass(route.handler):
@@ -74,13 +54,15 @@ class SchemaGenerator:
                     if not hasattr(route.handler, method):
                         continue
                     handler = getattr(route.handler, method)
-                    add_schema(schema, route.path, method, handler, route.tags)
+                    self._add_to_schema(
+                        schema, route.path, method, handler, route.tags
+                    )
             # if method, just loop through methods and add
             else:
                 for method in route.methods:
                     if method in ["HEAD"]:
                         continue
-                    add_schema(
+                    self._add_to_schema(
                         schema, route.path, method, route.handler, route.tags
                     )
 
@@ -122,6 +104,99 @@ class SchemaGenerator:
                 info += self._get_info(route.routes, path)
 
         return info
+
+    def _add_to_schema(
+        self,
+        schema: typing.Dict,
+        path: str,
+        method: str,
+        handler: typing.Callable,
+        tags: typing.List,
+    ):
+        """
+        Add the route documentation to OpenAPI docs
+        - automatically add path parameter documentation based on handler parameters
+          if not defined by user.
+        """
+
+        docs = self.get_docstirng(handler)
+        method = method.lower()
+
+        if len(docs) == 0:
+            return
+
+        # Auto add parameter documentation for each route
+        signature = inspect.signature(handler)
+        parameters = signature.parameters.copy()
+        param_names = list(signature.parameters.keys())
+
+        # if class, ignore first 2 params (self, request) else 1 (request)
+        if inspect.isclass(handler):
+            del parameters[param_names[0]]
+            del parameters[param_names[1]]
+            param_names = param_names[2:]
+        else:
+            del parameters[param_names[0]]
+            param_names = param_names[1:]
+
+        # auto add param documentation to route
+        if param_names:
+            if "parameters" not in docs:
+                docs["parameters"] = []
+
+            for param_name in param_names:
+                param_data = parameters.get(param_name)
+                schema_type = self._get_param_schema_types(
+                    param_data.annotation
+                )
+
+                # look for exists one and update fields
+                for param in docs["parameters"][:]:
+                    if param["name"] in parameters.keys():
+                        if "in" not in param:
+                            param["in"] = "path"
+
+                        # check if schema exists, if not add
+                        if "schema" not in param:
+                            param["schema"] = {}
+                            param["schema"][
+                                "type"
+                            ] = self._get_param_schema_types(
+                                param_data.annotation
+                            )
+
+                        # check if required exists, if not auto add
+                        if "required" not in param:
+                            if param_data.default is inspect.Parameter.empty:
+                                param["required"] = "true"
+
+                # if document is not exists at all, create one and add
+                param_doc = {}
+                param_doc["in"] = "path"
+                param_doc["name"] = param_name
+                param_doc["schema"] = {"type": schema_type}
+                if param_data.default is inspect.Parameter.empty:
+                    param_doc["required"] = "true"
+
+                docs["parameters"].append(param_doc)
+
+        if path not in schema["paths"]:
+            schema["paths"][path] = {}
+
+        schema["paths"][path][method] = docs
+        # do not override tags if user defined inside docstring
+        if tags and not schema["paths"][path][method].get("tags"):
+            schema["paths"][path][method]["tags"] = tags
+
+    def _get_param_schema_types(self, object_type: typing.Any) -> str:
+        if object_type == int:
+            return "integer"
+        elif object_type == bool:
+            return "boolean"
+        elif object_type == float:
+            return "number"
+        else:
+            return "string"
 
 
 class OpenAPIResponse(Response):
